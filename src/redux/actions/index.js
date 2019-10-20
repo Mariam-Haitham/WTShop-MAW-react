9a47bcdda9cdf1bbf1d70f73724849f9cb98ad8a
Alyaa AlOstad <alyaaalostad@hotmail.com>
2019-10-20 11:20:18 +0300

    done with checkout

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -3,3 +3,5 @@ export { fetchItems, filterItems } from "./itemsList";
 export { signup, login, logout, checkForExpiredToken } from "./auth";
 
 export { fetchProfile } from "./profile";
+
+export { addCart, removeCart, checkout } from "./cart";
